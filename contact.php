8bb5632f73ed48178685b9bbd12ab957c1623629
Arthur Kwabena Larbi Nathaniel <105647591+ArthurNathaniel@users.noreply.github.com>
2023-06-23 16:45:41 -0500

    changes

--- contact.php
+++ contact.php
@@ -6,8 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Contact Us | TLG Mining</title>
     <?php include 'cdn.php'; ?>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" 
+    crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="contact.css">
 </head>
@@ -15,18 +16,16 @@
 <body>
 
     <?php include 'navbar.php'; ?>
+
     <section>
         <div class="swiper  mySwiper">
-
             <div class="swiper-wrapper">
-
                 <div class="swiper-slide">
                     <img src="./images/home-one.jpg" alt="">
                     <div class="swiper-text">
                         <h4> Contact Us</h4>
                     </div>
                 </div>
-
             </div>
     </section>
 
@@ -46,10 +45,15 @@
             </div>
         </div>
     </section>
+
     <?php include 'contact-form.php'; ?>
+
     <?php include 'footer.php'; ?>
+
     <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+
     <!-- Initialize Swiper -->
+
     <script>
         var swiper = new Swiper(".mySwiper", {
             autoplay: true,
@@ -61,7 +65,11 @@
 
         });
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+     integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" 
+     crossorigin="anonymous"></script>
+     
 </body>
 
 </html>
